b2ce807476372629f09e52e009566aa78c1bcb58
ltl13 <66907671+ltl13@users.noreply.github.com>
2022-02-01 08:41:57 +0700

    feat(ui): add first part of navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,11 +2,13 @@ import React from "react";
 import { HiOutlineUserCircle } from "react-icons/hi";
 import { AiOutlineShoppingCart, AiOutlineSearch } from "react-icons/ai";
 import "./Navbar.sass";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navbar__title">
+        <div className="navbar__title__left"></div>
         <h1 className="navbar__title__text">glab</h1>
         <div className="navbar__title__icons">
           <i>
@@ -20,7 +22,28 @@ const Navbar = () => {
           </i>
         </div>
       </div>
-      <nav className="navbar__menu"></nav>
+      <nav className="navbar__menu">
+        <ul>
+          <li>
+            <Link to="/" className="menu__item">Feature</Link>
+          </li>
+          <li>
+            <Link to="/" className="menu__item">Footwear</Link>
+          </li>
+          <li>
+            <Link to="/" className="menu__item">Apparel</Link>
+          </li>
+          <li>
+            <Link to="/" className="menu__item">Accessories</Link>
+          </li>
+          <li>
+            <Link to="/" className="menu__item">Used</Link>
+          </li>
+          <li>
+            <Link to="/" className="menu__item">Sale</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
